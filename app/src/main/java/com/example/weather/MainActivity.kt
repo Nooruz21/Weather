bd05cb701f69ead08d69c44e6042156cf903468b
Nooruz21 <awbawb2129@gmail.com>
2023-04-07 20:19:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weather.presentation.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+
+            }
+        }
+    }
+}
+
+
