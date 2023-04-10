f0bd3553a92f2746f3b73f43a392ab0d4812d88f
Nooruz21 <awbawb2129@gmail.com>
2023-04-10 14:16:56 +0600

    add WeatherCard.kt

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -1,15 +1,8 @@
-package com.example.weather
+package com.example.weather.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.weather.presentation.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +10,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherTheme {
-
             }
         }
     }
