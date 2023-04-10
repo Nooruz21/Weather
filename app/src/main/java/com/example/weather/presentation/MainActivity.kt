b29047cdc250ce5d3c8ae84ad893263874186733
Nooruz21 <awbawb2129@gmail.com>
2023-04-10 15:08:08 +0600

    add WeatherDataDisplay.kt

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.weather.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.example.weather.presentation.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
